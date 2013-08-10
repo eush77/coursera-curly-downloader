ffe78a7baad658891b1358324170d7e0ae55fd72
eush77 <eush77@gmail.com>
2013-08-11 03:25:43 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Coursera: Unwatched Resources
+=============================
+This Chrome extension activates on Coursera lecture pages only.
+
+In a click, it scans through the list and generates Curl configuration file which enables fast batch download of unwatched resources available. Config opens in a new tab ready to be copied.
+
+## Using with Curl
+Configuration file includes required authentication cookie and can be launched either way:
+
+    cat >config
+    curl --config config
+
+    cat |curl --config -
---
+++ background.js
@@ -0,0 +1,41 @@
+var matcher = new RegExp('^https?://class.coursera.org/.*/lecture/index/?$');
+var courseraMain = function(url) {
+    return url.split('://')[0] + '://coursera.org';
+};
+
+var authCookie = 'CAUTH';
+var curlConfigPrefix = function(authCookie, authValue) {
+    return ['remote-name-all',
+            'remote-header-name',
+            'location',
+            'cookie = ' + authCookie + '=' + authValue].join('<br/>') + '<br/><br/>';
+};
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.cookies.get({
+        url: courseraMain(tab.url),
+        name: authCookie
+    }, function(cookie) {
+        var script = function(configPrefix) {
+            // Target webpage context!
+            var links = [].slice.call(document.getElementsByClassName('unviewed')).map(function(li) {
+                return [].slice.call(li.children[1].children).map(function(a) {
+                    return a.href;
+                });
+            }).reduce(function(a, b) {
+                return a.concat(b);
+            });
+            links = 'url = ' + links.join('<br/>url = ');
+            window.open('javascript: document.write("' + configPrefix + links + '")');
+        };
+        chrome.tabs.executeScript(null, {
+            code: '!' + script + '("' + curlConfigPrefix(authCookie, cookie.value) + '")'
+        });
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (matcher.test(tab.url)) {
+        chrome.pageAction.show(tabId);
+    }
+});
