2d7f63a8c987ffe649332f3b567c84fba102be36
eush77 <eush77@gmail.com>
2013-08-27 00:36:30 +0400

    Checkboxes for manual selection

--- background.js
+++ background.js
@@ -1,42 +1,38 @@
 var matcher = new RegExp('^https?://class\.coursera\.org/.*/lecture/index/?$');
+var AUTH_COOKIE = 'session';
 
-var authCookie = 'session';
-var curlConfigPrefix = function(authCookie, authValue) {
+/** Curl config prefix
+ * @param {String} authValue Authorization cookie value.
+ * @returns {String}
+ */
+var curlConfigPrefix = function(authValue) {
     return ['remote-name-all',
             'remote-header-name',
             'location',
-            'cookie: ' + authCookie + '=' + authValue].join('<br/>') + '<br/><br/>';
+            'cookie: ' + AUTH_COOKIE + '=' + authValue].join('<br/>') + '<br/><br/>';
 };
 
+// Set page-action button up
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.cookies.get({
         url: tab.url,
-        name: authCookie
+        name: AUTH_COOKIE
     }, function(cookie) {
-        var script = function(configPrefix) {
-            // Target webpage context!
-            var links = [].slice.call(document.getElementsByClassName('unviewed')).map(function(li) {
-                return [].slice.call(li.children[1].children).map(function(a) {
-                    return a.href;
-                });
-            }).reduce(function(a, b) {
-                return a.concat(b);
-            }, []);
+        var configPrefix = curlConfigPrefix(cookie.value);
+        chrome.tabs.sendMessage(tab.id, 'extract', function(links) {
             if (links.length) {
                 links = 'url: ' + links.join('<br/>url: ');
                 // Links are urldecoded when followed
                 window.open(('data:text/html, ' + configPrefix + links).replace(/%/g, '%25'));
             }
             else {
-                alert('All the lectures marked as watched!');
+                alert('No items selected!');
             }
-        };
-        chrome.tabs.executeScript(null, {
-            code: '!' + script + '("' + curlConfigPrefix(authCookie, cookie.value) + '")'
         });
     });
 });
 
+// Show page-action button
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (matcher.test(tab.url)) {
         chrome.pageAction.show(tabId);
---
+++ injection.js
@@ -0,0 +1,97 @@
+var CHECKBOX_SCALE = 3/4;
+var RIGHT_PADDING_PORTION = 1/4;
+var OPACITY = 0.3;
+
+var section = document.getElementsByClassName('course-item-list')[0];
+var lectures = section.getElementsByTagName('li');
+
+// Sparse array of selected resources for each lecture
+var selected = [];
+
+/** Return resource bar for given lectures list item.
+ * @param {HTMLLIElement} li
+ * @returns {HTMLDivElement}
+ */
+var resourcesBar = function(li) {
+    return li.getElementsByClassName('course-lecture-item-resource')[0];
+};
+
+/** Create checkbox with the given ID
+ * @param {Number} index
+ * @returns {HTMLInputElement}
+ */
+var Checkbox = (function(li) {
+    // Compute values
+    var resbar = resourcesBar(li);
+    var icon = resbar.getElementsByTagName('i')[0];
+    var size = icon.offsetHeight * CHECKBOX_SCALE;
+    var shiftRight = parseInt(window.getComputedStyle(li).paddingRight) * (1 - RIGHT_PADDING_PORTION);
+    var shiftBottom = parseInt(window.getComputedStyle(resbar).paddingTop)
+        + parseInt(window.getComputedStyle(icon).paddingTop);
+    // Make prototype
+    var checkboxTemplate = document.createElement('input');
+    checkboxTemplate.type = 'checkbox';
+    checkboxTemplate.style.cssFloat = 'right';
+    checkboxTemplate.style.width = checkboxTemplate.style.height = size + 'px';
+    checkboxTemplate.style.marginRight = -shiftRight + 'px'
+    checkboxTemplate.style.marginTop = shiftBottom + 'px';
+    checkboxTemplate.style.opacity = OPACITY;
+    checkboxTemplate.className = 'curly-checkbox';
+    // Return constructor
+    return function(index) {
+        var checkbox = checkboxTemplate.cloneNode();
+        checkbox.dataset.index = index; // Set index attribute
+        return checkbox;
+    };
+}(lectures[0]));
+
+// Error handling
+var error = function(e) {
+    console.error(e.name + ': ' + e.message);
+};
+
+var InvalidMessageError = function(message) {
+    var error = new Error('"' + message + '"');
+    error.name = 'Invalid message';
+    return error;
+};
+
+// Create and embed checkboxes
+for (var index = 0; index < lectures.length; ++index) {
+    var li = lectures[index];
+    li.insertBefore(Checkbox(index), li.lastElementChild);
+}
+
+// Handle clicks on checkboxes
+section.addEventListener('change', function(event) {
+    if (event.target.classList.contains('curly-checkbox')) {
+        var checkbox = event.target;
+        if (checkbox.checked) {
+            var links = [].slice.call(resourcesBar(checkbox.parentNode).children).map(function(a) {
+                return a.href;
+            });
+        }
+        else {
+            var links = []; // Can still be passed to concat
+        }
+        selected[checkbox.dataset.index] = links;
+    }
+});
+
+// Listen for messages
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    switch (message) {
+    case 'extract':
+        // Extract resource links for marked lectures.
+        var arrayOfArrays = Object.keys(selected).filter(isFinite).sort(function(a, b) {
+            return a - b;
+        }).map(function(key) {
+            return selected[key];
+        });
+        array = [].concat.apply([], arrayOfArrays);
+        sendResponse(array);
+        break;
+    default:
+        error(InvalidMessageError(message));
+    }
+});
